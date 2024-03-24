bf99ef46d392040aeef0dc477693879d4bbbd082
«Ваше <fr33lancemail@gmail.com>
2024-03-24 19:00:35 +0100

    modification model Product
    enum

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   validates :title, presence: true
   validates :price, presence: true
+
+  enum hit: { hit: 1, not_hit: 0 } # Указываем варианты значений полей hit и not_hit в таблице products.
 end
